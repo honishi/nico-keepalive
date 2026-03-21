e321b66d9653a01e2a8772233b1d91eaf852051a
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2026-03-22 08:54:00 +0900

    Refactor content monitor logic

---
+++ src/content/types.ts
@@ -0,0 +1,22 @@
+export type StallReason = "currentTime" | "deepCheck";
+
+export type ProgramContext = {
+  programId?: string;
+  providerName?: string;
+};
+
+export type MonitorRuntimeSettings = {
+  enabled: boolean;
+  deepCheckModeEnabled: boolean;
+  deepCheckThresholdMs: number;
+  monitorOverlayEnabled: boolean;
+  debugWarmupEnabled: boolean;
+  debugCurrentTimeCheckEnabled: boolean;
+  debugDeepCheckEnabled: boolean;
+};
+
+export type MonitorLogger = {
+  debug: (message: string) => void;
+  warn: (message: string) => void;
+  trace: (message: string) => void;
+};
